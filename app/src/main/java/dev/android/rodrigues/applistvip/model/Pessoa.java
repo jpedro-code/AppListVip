6c7e0f78f32ca770cc15395ff57767b18a0395a8
Joao Pedro <jpedro.rpessoa@gmail.com>
2023-06-27 21:53:19 -0300

    Salvando os elementos do formulário e atribuindo eles a uma instância de pessoa.

--- app/src/main/java/dev/android/rodrigues/applistvip/model/Pessoa.java
+++ app/src/main/java/dev/android/rodrigues/applistvip/model/Pessoa.java
@@ -6,8 +6,37 @@ public class Pessoa{
     private String nomeDoCurso;
     private String phone;
 
-    public Pessoa(String nome, String sobrenome){
-        nome = this.nome;
-        sobrenome = this.sobrenome;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNomeDoCurso() {
+        return nomeDoCurso;
+    }
+
+    public void setNomeDoCurso(String nomeDoCurso) {
+        this.nomeDoCurso = nomeDoCurso;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
--- app/src/main/java/dev/android/rodrigues/applistvip/view/MainActivity.java
+++ app/src/main/java/dev/android/rodrigues/applistvip/view/MainActivity.java
@@ -3,6 +3,9 @@ package dev.android.rodrigues.applistvip.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import dev.android.rodrigues.applistvip.R;
@@ -10,12 +13,61 @@ import dev.android.rodrigues.applistvip.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
     Pessoa pessoa;
+    EditText editNome;
+    EditText editSobrenome;
+    EditText editNomeCurso;
+    EditText editTelefone;
+
+    Button btnSalvar;
+    Button btnLimpar;
+    Button btnFinalizar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(),"Bem Vindo ao seu app listVip",Toast.LENGTH_LONG).show();
-        pessoa = new Pessoa("Joao Pedro","Rodrigues Pessoa");
+
+        pessoa = new Pessoa();
+        editNome = findViewById(R.id.name_edit);
+        editSobrenome = findViewById(R.id.sobrenome_edit);
+        editNomeCurso = findViewById(R.id.curso_edit);
+        editTelefone = findViewById(R.id.phone_ed);
+
+        btnSalvar = findViewById(R.id.button_salvar);
+        btnLimpar = findViewById(R.id.button_limpar);
+        btnFinalizar = findViewById(R.id.button_finalizar);
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editNome.setText("");
+                editSobrenome.setText("");
+                editTelefone.setText("");
+                editNomeCurso.setText("");
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pessoa.setNome(editNome.getText().toString());
+                pessoa.setSobrenome(editSobrenome.getText().toString());
+                pessoa.setNomeDoCurso(editNomeCurso.getText().toString());
+                pessoa.setPhone(editTelefone.getText().toString());
+                Toast.makeText(MainActivity.this, "Seus Dados Foram Salvos Corretamente "+ pessoa.getNome(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
+
+
 }
